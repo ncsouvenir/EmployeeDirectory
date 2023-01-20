45f56bd8ee49f2c26c971d4f90b46ec16081e112
Nicole Souvenir <ncsouvenir@gmail.com>
2023-01-20 13:04:25 -0500

    Initial Commit

---
+++ Employee Directory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employee Directory
+//
+//  Created by Nic Souvenir on 1/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
